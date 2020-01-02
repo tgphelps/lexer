4c4b77fa0f316563f5d8a40e0cfb58776d43e898
Terry Phelps <tgphelps50@gmail.com>
2020-01-02 15:47:07 -0500

    save

--- lexer.go
+++ lexer.go
@@ -13,12 +13,18 @@ const (
 )
 
 type token struct {
-	typ tokenType
+	// typ tokenType
+	// val string
+	n int
 	val string
 }
 
 type lexer struct {
 	tokenCh chan token
+}
+
+func delay(n time.Duration) {
+	time.Sleep(n * time.Millisecond)
 }
 
 func newLexer(ch chan token) *lexer {
@@ -30,12 +36,22 @@ func newLexer(ch chan token) *lexer {
 
 func (l *lexer) run() {
 	fmt.Println("lexer running...")
-} 
+	l.tokenCh <- token{n: 1, val: "one"}
+	close(l.tokenCh)
+}
 
 func main() {
 	ch := make(chan token)
 	l := newLexer(ch)
 	fmt.Println("lexer:", l)
 	go l.run()
-	time.Sleep(100 * time.Millisecond)
+	delay(100)
+	tok, ok := <-ch
+	fmt.Println("channel data:", tok, "ok:", ok)
+	delay(100)
+	tok, ok = <-ch
+	fmt.Println("channel data:", tok, "ok:", ok)
+	delay(100)
+	tok, ok = <-ch
+	fmt.Println("channel data:", tok, "ok:", ok)
 }
