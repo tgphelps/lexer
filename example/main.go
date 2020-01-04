cd744a0aa4c18bafddee26a36cc5e4df96f4d788
Terry Phelps <tgphelps50@gmail.com>
2020-01-03 19:52:36 -0500

    save

--- example/main.go
+++ example/main.go
@@ -5,49 +5,76 @@ import (
 	"fmt"
 	"lexer"
 	"os"
+	"strings"
 	"time"
+	"unicode"
 )
 
 // type stateFn func(*lexer.Lexer) stateFn
+type charTestFn func(rune) bool
 
 // The first two of these must match those in lexer.go
 const (
 	TokError lexer.TokenType = iota
 	TokEof
 	TokChar
+	TokAlphas
+	TokDigits
 )
 
 func main() {
-    ch := make(chan lexer.Token)
-    src := bufio.NewReader(os.Stdin)
-    l := lexer.NewLexer(ch, src, lexState)
+	ch := make(chan lexer.Token)
+	src := bufio.NewReader(os.Stdin)
+	l := lexer.NewLexer(ch, src, lexState)
 
-    go l.Run()
-    delay(100)
-    for {
-        tok, ok := <-ch
-        if !ok {
-            // fmt.Println("got NOT ok:", ok)
-            break
-        }
-        fmt.Println("channel data:", tok, "ok:", ok)
-        delay(100)
-    }
+	go l.Run()
+	delay(100)
+	for {
+		tok, ok := <-ch
+		if !ok {
+			// fmt.Println("got NOT ok:", ok)
+			break
+		}
+		fmt.Println("channel data:", tok, "ok:", ok)
+		delay(100)
+	}
 }
 
 func lexState(l *lexer.Lexer) lexer.StateFn {
 	fmt.Println("start lexState")
 	for {
-		r :=  l.Next()
+		r := l.Next()
+		// fmt.Printf("rune = %q\n", r)
 		if r == lexer.EofRune {
 			l.Emit(lexer.Token{Type: TokEof})
 			break
 		} else {
-			t := lexer.Token{Type: TokChar, Val: string(r)}
+			var t lexer.Token
+			if unicode.IsLetter(r) {
+				s := collect(l, unicode.IsLetter, r)
+				t = lexer.Token{Type: TokAlphas, Val: s}
+			} else {
+				t = lexer.Token{Type: TokChar, Val: string(r)}
+			}
 			l.Emit(t)
 		}
 	}
 	return nil
+}
+
+func collect(l *lexer.Lexer, testFunc charTestFn, r rune) string {
+	var b strings.Builder
+	b.WriteRune(r)
+	for {
+		r := l.Next()
+		if testFunc(r) {
+			b.WriteRune(r)
+		} else {
+			l.UnNext()
+			break
+		}
+	}
+	return b.String()
 }
 
 func delay(d time.Duration) {
--- lexer.go
+++ lexer.go
@@ -3,27 +3,28 @@ package lexer
 import (
 	"bufio"
 	"fmt"
+	"log"
 )
 
 type TokenType int
 
 const (
-    TokError TokenType = iota
-    TokEof
+	TokError TokenType = iota
+	TokEof
 )
 
 type Token struct {
 	// typ TokenType
 	// val string
 	Type TokenType
-	Val string
+	Val  string
 }
 
 type StateFn func(*Lexer) StateFn
 
 type Lexer struct {
-	tokenCh chan Token
-	src     *bufio.Reader
+	tokenCh    chan Token
+	src        *bufio.Reader
 	startState StateFn
 }
 
@@ -39,13 +40,13 @@ func (t Token) String() string {
 	if len(t.Val) > 10 {
 		return fmt.Sprintf("%.10q...", t.Val)
 	}
-	return fmt.Sprintf("%q", t.Val)
+	return fmt.Sprintf("<%d, %q>", t.Type, t.Val)
 }
 
 func NewLexer(ch chan Token, src *bufio.Reader, start StateFn) *Lexer {
 	l := &Lexer{
-		tokenCh: ch,
-		src: src,
+		tokenCh:    ch,
+		src:        src,
 		startState: start,
 	}
 	return l
@@ -53,11 +54,20 @@ func NewLexer(ch chan Token, src *bufio.Reader, start StateFn) *Lexer {
 
 func (l *Lexer) Next() rune {
 	r, _, err := l.src.ReadRune()
-	if err  != nil {
-		// EOF or error
-		return  EofRune
+	if err != nil {
+		if err.Error() == "EOF" {
+			return EofRune
+		}
+		log.Panic("ReadRune got error")
 	}
 	return r
+}
+
+func (l *Lexer) UnNext() {
+	err := l.src.UnreadRune()
+	if err != nil {
+		log.Panic("UnreadRune got error")
+	}
 }
 
 func (l *Lexer) Emit(t Token) {
@@ -69,21 +79,20 @@ func (l *Lexer) Run() {
 	for state := l.startState; state != nil; {
 		state = state(l)
 	}
-/***
-	for {
-		r, _, err := l.src.ReadRune()
-		if  err != nil {
-			// This happens at EOF
-			fmt.Println("error", err)
-			if err.Error() == "EOF" {
-				fmt.Println("got EOF")
+	/***
+		for {
+			r, _, err := l.src.ReadRune()
+			if  err != nil {
+				// This happens at EOF
+				fmt.Println("error", err)
+				if err.Error() == "EOF" {
+					fmt.Println("got EOF")
+				}
+				break
 			}
-			break
+			fmt.Printf("lexer read: %v %q\n", r, r)
+			l.tokenCh <- Token{r: r, val: string(r)}
 		}
-		fmt.Printf("lexer read: %v %q\n", r, r)
-		l.tokenCh <- Token{r: r, val: string(r)}
-	}
-***/
+	***/
 	close(l.tokenCh)
 }
-
